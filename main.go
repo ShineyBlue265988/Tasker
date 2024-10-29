2eb4625d5c8cd70f02c370592f2c57dd8ec5069c
ShineyBlue265988 <jamesbin265988@gmail.com>
2024-10-29 01:46:46 -0600

    feat: add function

--- main.go
+++ main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"context"
-	"log"
-	"os"
-
+	"errors"
 	"github.com/urfave/cli/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
 )
 
 var collection *mongo.Collection
 var CTX = context.TODO()
 
+type Task struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	Text      string             `bson:"text"`
+	Completed bool               `bson:"completed"`
+}
+
+func createTask(task *Task) error {
+	_, err := collection.InsertOne(CTX, task)
+	return err
+}
 func init() {
 	client, err := mongo.Connect(CTX, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -28,9 +42,31 @@ func init() {
 
 func main() {
 	app := &cli.App{
-		Name:     "tasker",
-		Usage:    "A simple CLI program to manage your tasks",
-		Commands: []*cli.Command{},
+		Name:  "tasker",
+		Usage: "A simple CLI program to manage your tasks",
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(c *cli.Context) error {
+					str := c.Args().First()
+					if str == "" {
+						return errors.New("Cannot add an empty task")
+					}
+
+					task := &Task{
+						ID:        primitive.NewObjectID(),
+						CreatedAt: time.Now(),
+						UpdatedAt: time.Now(),
+						Text:      str,
+						Completed: false,
+					}
+
+					return createTask(task)
+				},
+			},
+		},
 	}
 
 	err := app.Run(os.Args)
